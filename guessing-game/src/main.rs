057a385c9f69ca5dc48e7e0e58f1471abbbb97d4
hindawi <ahmedhindawi2001@gmail.com>
2023-04-23 03:30:17 +0200

    first part done

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,4 +1,9 @@
+use std::io;
+
+#[allow(unused)]
 fn main() {
-    println!("Hello, world!");
-    println!("Hello, world!");
+    println!("Guess a number");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    println!("you guessed : {guess}");
 }
